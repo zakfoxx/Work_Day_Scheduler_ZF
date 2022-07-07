393fb431fa8938e3f58d6d74afd81ffb38f39677
Zechariah Foxx <zak.foxx@gmail.com>
2022-07-07 18:59:58 -0400

    create button functions

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, "0");
-var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-var yyyy = today.getFullYear();
-var days = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-var day = days[now.getDay];
-today = mm + "/" + dd + "/" + yyyy;
-document.write(today);
+function getPlan() {
+  return localStorage.getItem("plan");
+}
 
 // make able to save and load to local storage
 // apply past present and future classes based on time hour represents
 // write loop that loops over each div - compares hour of row to hour from moment
+
+$(".saveBtn").on("click", function () {
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+
+  console.log(value, time);
+
+  // save time and value to localstorage
+});
