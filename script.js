d45ff7f4d45127ae7e5f56a021af8149b31627a3
Zechariah Foxx <zak.foxx@gmail.com>
2022-07-07 21:34:07 -0400

    create localStorage

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function getPlan() {
-  return localStorage.getItem("plan");
+  //  return localStorage.getItem("plan");
+  $("#hour-9").children("textarea").val(localStorage.getItem("hour-9"));
+  $("#hour-10").children("textarea").val(localStorage.getItem("hour-10"));
+  $("#hour-11").children("textarea").val(localStorage.getItem("hour-11"));
+  $("#hour-12").children("textarea").val(localStorage.getItem("hour-12"));
+  $("#hour-13").children("textarea").val(localStorage.getItem("hour-13"));
+  $("#hour-14").children("textarea").val(localStorage.getItem("hour-14"));
+  $("#hour-15").children("textarea").val(localStorage.getItem("hour-15"));
+  $("#hour-16").children("textarea").val(localStorage.getItem("hour-16"));
+  $("#hour-17").children("textarea").val(localStorage.getItem("hour-17"));
+}
+getPlan();
+function savePlan(thePlan, hour) {
+  localStorage.setItem(hour, thePlan);
 }
 
 // make able to save and load to local storage
@@ -10,7 +23,7 @@ $(".saveBtn").on("click", function () {
   var value = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
 
-  console.log(value, time);
+  savePlan(value, time);
 
   // save time and value to localstorage
 });
